077dd880b2fc7fdc16db7d8b8fcf87212d6fbe6f
Lassito-06 <Lassana.Hamady.Sow@BL-Excellence.com>
2025-09-19 02:56:59 +0100

    Integration Extraction

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -3,16 +3,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from . import ai_views  
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/ai-performance/', ai_views.ai_performance_hub, name='ai_performance_hub'),
     path('admin/ai-performance/metadata/', ai_views.metadata_learning_dashboard, name='metadata_learning_dashboard'),
     path('admin/', admin.site.urls),
-    path('', include('rawdocs.urls')),
+    path('rawdocs/', include('rawdocs.urls')),
     path('expert/', include('expert.urls')),
     path('client/', include('client.urls', namespace='client')),
     path('client/submissions/', include(('client.submissions.ctd_submission.urls', 'ctd_submission'), namespace='ctd_submission')),
     path('chatbot/', include('chatbot.urls')),
+    path('', RedirectView.as_view(url='/rawdocs/', permanent=False)),
+    path('documents/', include(('documents.urls', 'documents'), namespace='documents')),
 ]
 
 if settings.DEBUG:
