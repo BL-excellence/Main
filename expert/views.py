42c06e3aba2dcf355c165ce6915b572c94b6c27d
Lassito-06 <Lassana.Hamady.Sow@BL-Excellence.com>
2025-10-20 03:47:29 +0100

    Add drag-and-drop PDF upload UI

--- expert/views.py
+++ expert/views.py
@@ -3908,7 +3908,7 @@ def expert_save_manual_annotation(request):
             validated_by=request.user,
             validated_at=timezone.now(),
             created_by=request.user,
-            source='expert'
+            source='expert_manual'
         )
 
         # LOG (facultatif) ...
